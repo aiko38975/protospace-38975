ce9afb195f9f7a78d079719fb6861a5ea8ee1d46
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-06 11:04:02 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes.includes(:user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :profile, :occupation, :position)
+  end
+
+end
